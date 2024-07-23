b45339726343ecca0664d497aec6ab9daf2aec80
Danh-m1 <danh2k@nexondv.com>
2024-07-23 20:45:11 +0700

    lucia Next-validate cookie

--- src/lib/lucia-auth.ts
+++ src/lib/lucia-auth.ts
@@ -1,7 +1,9 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
-import { Lucia } from "lucia";
+import { Lucia, Session, User } from "lucia";
 import prisma from "./prisma";
 import { Prisma } from "@prisma/client";
+import { cache } from "react";
+import { cookies } from "next/headers";
 
 // https://lucia-auth.com/database/prisma
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
@@ -59,3 +61,53 @@ type DatabaseUserAttributes = Prisma.UserGetPayload<{
 //   interface DatabaseUserAttributes extends Pick<Prisma.UserGetPayload<{}>,
 //   'id' | 'username' | 'displayName' | 'avatarUrl' | 'googleId'
 // > {}
+
+// https://lucia-auth.com/guides/validate-session-cookies/nextjs-app
+// type của Lucia, ko phải Prisma
+
+export type TLuciaValidate =
+  | {
+      user: User;
+      session: Session;
+    }
+  | {
+      user: null;
+      session: null;
+    };
+
+export const validateRequest = cache(async (): Promise<TLuciaValidate> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+
+  if (!sessionId)
+    return {
+      user: null,
+      session: null,
+    };
+
+  // ko destruct ra để tiện return khớp type
+  const result = await lucia.validateSession(sessionId);
+  const { session } = result;
+
+  try {
+    if (session?.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+    if (!session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+  } catch {
+    // Next.js throws error when attempting to set cookies when rendering page
+  }
+
+  return result;
+});
