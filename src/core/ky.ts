74a1141270754a4d2701c433795fd986ea1be81b
Danh-m1 <danh2k@nexondv.com>
2024-08-01 20:49:06 +0700

    KY - replace axios and native fetch

---
+++ src/core/ky.ts
@@ -0,0 +1,13 @@
+// https://github.com/sindresorhus/ky?tab=readme-ov-file#kycreatedefaultoptions
+// https://github.com/sindresorhus/ky?tab=readme-ov-file#parsejson
+import ky from "ky";
+
+const kyInstance = ky.create({
+  parseJson: text =>
+    JSON.parse(text, (key, value) => {
+      if (key.endsWith("At")) return new Date(value);
+      return value;
+    }),
+});
+
+export default kyInstance;
--- src/module/for-you-feed/for-you-feed-client.tsx
+++ src/module/for-you-feed/for-you-feed-client.tsx
@@ -3,24 +3,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
+import kyInstance from "@core/ky";
 import { PostWithUser } from "@core/prisma/post.prisma";
 
 import { PostItem } from "@module/post-item";
 
+const _nativeFetch_ForYouFeed = async () => {
+  const res = await fetch("/api/posts/for-you");
+  if (!res.ok) {
+    throw Error(`Request failed with status code ${res.status}`);
+  }
+
+  // Đối với các data primitive đơn giản thì .json() ok
+  // tuy nhiên chú ý post có data dạng Date()
+  // .json() bình thường logic xử lý tiếp vào date sẽ sai
+  return res.json();
+};
+
 export function ForYouFeedClient() {
   const query = useQuery<PostWithUser[]>({
     queryKey: ["post-feed", "for-you"],
-    queryFn: async () => {
-      const res = await fetch("/api/posts/for-you");
-      if (!res.ok) {
-        throw Error(`Request failed with status code ${res.status}`);
-      }
-
-      // Đối với các data primitive đơn giản thì .json() ok
-      // tuy nhiên chú ý post có data dạng Date()
-      // .json() bình thường logic xử lý tiếp vào date sẽ sai
-      return res.json();
-    },
+    queryFn: kyInstance.get("/api/posts/for-you").json<PostWithUser[]>,
   });
 
   if (query.status === "pending") {
