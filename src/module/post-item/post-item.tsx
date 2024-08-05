febe77065d82c8809bf6e2ac3f444bfa06b26046
Danh Tran <danh2k@nexondv.com>
2024-08-05 15:45:10 +0700

    post-more-dropdown with user delete permission check

--- src/module/post-item/post-item.tsx
+++ src/module/post-item/post-item.tsx
@@ -4,6 +4,7 @@ import { formatRelativeDate } from "@core/helper/time.utils";
 import { PostWithUser } from "@core/prisma/post.prisma";
 
 import { UserAvatar } from "@module/app-global/navbar";
+import PostMoreButton from "@module/post-more";
 
 interface PostProps {
   post: PostWithUser;
@@ -11,25 +12,33 @@ interface PostProps {
 
 export function PostItem({ post }: PostProps) {
   return (
-    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
-      <div className="flex flex-wrap gap-3">
-        <Link href={`/users/${post.user.username}`}>
-          <UserAvatar avatarUrl={post.user.avatarUrl} />
-        </Link>
-        <div>
-          <Link
-            href={`/users/${post.user.username}`}
-            className="block font-medium hover:underline"
-          >
-            {post.user.displayName}
+    <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <Link href={`/users/${post.user.username}`}>
+            <UserAvatar avatarUrl={post.user.avatarUrl} />
           </Link>
-          <Link
-            href={`/posts/${post.id}`}
-            className="block text-sm text-muted-foreground hover:underline"
-          >
-            {formatRelativeDate(post.createdAt)}
-          </Link>
+          <div>
+            <Link
+              href={`/users/${post.user.username}`}
+              className="block font-medium hover:underline"
+            >
+              {post.user.displayName}
+            </Link>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
+
+        {/* TODO: Bug khi đang click thì dropdown menu bị ẩn đi  */}
+        <PostMoreButton
+          post={post}
+          className="opacity-0 transition-opacity group-hover/post:opacity-100"
+        />
       </div>
       {/* https://developer.mozilla.org/en-US/docs/Web/CSS/white-space */}
       <div className="whitespace-pre-line break-words">{post.content}</div>
---
+++ src/module/post-more/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { MoreHorizontal, Trash2 } from "lucide-react";
+import { useState } from "react";
+
+import { PostWithUser } from "@core/prisma/post.prisma";
+
+import { useSession } from "@module/app-provider";
+import { Button } from "@module/app-shadcn/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@module/app-shadcn/dropdown-menu";
+
+import { DeletePostDialog } from "@module/delete-post/";
+
+interface PostMoreButtonProps {
+  post: PostWithUser;
+  className?: string;
+}
+
+export default function PostMoreButton({
+  post,
+  className,
+}: PostMoreButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const { user } = useSession();
+
+  if (post.user.id !== user.id) return null;
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2 className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostDialog
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+}
