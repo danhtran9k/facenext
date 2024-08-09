821095294c375c0c706975e81ffc6fb85db1877d
Danh Tran <danh2k@nexondv.com>
2024-08-09 17:01:01 +0700

    user profile edit dialog with api-action-query-mutate

---
+++ src/app/api/users/update/update-user-profile.action.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { validateRequest } from "@app/api/_core/lucia-auth";
+import prisma from "@app/api/_core/prisma";
+
+import { userDataSelect } from "../user.query";
+
+import {
+  updateUserProfileSchema,
+  UpdateUserProfileValues,
+} from "./update-user-profile.dto";
+
+export async function updateUserProfile(values: UpdateUserProfileValues) {
+  const validatedValues = updateUserProfileSchema.parse(values);
+
+  const { user } = await validateRequest();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: validatedValues,
+    select: userDataSelect(user.id),
+  });
+
+  return updatedUser;
+}
